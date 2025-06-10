1f872e426d160f9e748d68614f236252cf9e1d29
muidaoqui <muidao156@gmail.com>
2025-06-10 18:49:29 +0700

    test antd

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,39 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './index.css';
+// // import Home from './customer/home';
+// import Navbar from './customer/navbar';
+// import Cinema from './customer/cinema';
+// import Products from './customer/product';
+// import News from './customer/news';
+// import Account from './customer/account'; 
+// import HomeAdmin from './admin/home'
+// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <Router>
+//       {/* <Navbar /> */}
+//       <HomeAdmin />
+//       <Routes>
+//         {/* <Route path="/" element={<Home />} />
+//         <Route path="/cinema" element={<Cinema />} />
+//         <Route path="/products" element={<Products />} />
+//         <Route path="/news" element={<News />} />
+//         <Route path="/account" element={<Account />} />
+//         <Route path="*" element={<Navigate to="/" />} /> */}
+//       </Routes>
+//     </Router>
+//   </React.StrictMode>
+// );
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import HomeAdmin from './admin/home'; // đảm bảo tên file đúng là home.jsx
 import './index.css';
-import Home from './customer/home';
-import Navbar from './customer/navbar';
-import Cinema from './customer/cinema';
-import Products from './customer/product';
-import News from './customer/news';
-import Account from './customer/account'; 
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cinema" element={<Cinema />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/account" element={<Account />} />
-        {/* Redirect any unknown paths to home */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <HomeAdmin />
   </React.StrictMode>
 );
